7b5da34c0f0b8a33ce3254b09b14b27f3248afbc
Chevdor <chevdor@gmail.com>
2022-01-10 11:49:35 +0100

    Update to clap v3

--- src/opts.rs
+++ src/opts.rs
@@ -1,11 +1,10 @@
-use clap::{crate_authors, crate_version, AppSettings, Clap};
+use clap::{crate_authors, crate_version, Parser};
 use std::path::PathBuf;
 
 /// Command line utility for the tera templating engine. You need to provide a template using the tera syntax
 /// as well as some data (various format are supported).
-#[derive(Clap, Debug)]
+#[derive(Debug, Parser)]
 #[clap(version = crate_version!(), author = crate_authors!())]
-#[clap(setting = AppSettings::ColoredHelp)]
 pub struct Opts {
 	/// Location of the template.
 	#[clap(short, long)]
