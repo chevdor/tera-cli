4f971f4277c8e6284986b00b160d4ab3478b25dc
TeraBot <chevdor@users.noreply.github.com>
2024-06-05 10:55:39 +0000

    build: new homebrew formula for v0.4.0

--- Formula/tera.rb
+++ Formula/tera.rb
@@ -1,9 +1,9 @@
 class Tera < Formula
   desc "A command line utility written in Rust to render templates using the tera templating engine"
   homepage "https://github.com/chevdor/tera-cli"
-  url "https://github.com/chevdor/tera-cli/releases/download/v0.2.4/tera-macos-v0.2.4.tar.gz"
-  sha256 "1ef7cacb6d88000695eac33be94f90d68aaab4bbb4c7c04388750e6c1d9b9970"
-  version "0.2.4"
+  url "https://github.com/chevdor/tera-cli/releases/download/v0.4.0/tera-macos-v0.4.0.tar.gz"
+  sha256 "932c80baaf11b37ebe25a86b80d6ccd4c7f4cd308d188f5e2a29582959e99dc3"
+  version "0.4.0"
 
   def install
     bin.install "tera"
