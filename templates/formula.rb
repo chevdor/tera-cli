cda2c5df44815ed51caf4e4cb148745b99bac2b4
hituzi no sippo <dev@hituzi-no-sippo.me>
2024-09-25 05:13:25 +0900

    ci(homebrew): add arm support in Homebrew

--- templates/formula.rb
+++ templates/formula.rb
@@ -2,10 +2,14 @@
 {%- set HOMEPAGE = HOMEPAGE | default(value=SITE ~ "/" ~ REPO) -%}
 
 class {{ NAME }} < Formula
+  arch arm: "aarch64", intel: 'x86_64'
+
   desc "{{ DESCRIPTION }}"
   homepage "{{ HOMEPAGE }}"
-  url "{{ REPO_URL }}/releases/download/v{{ VERSION }}/{{ ARCHIVE }}"
-  sha256 "{{ SHA256 }}"
+  url "{{ REPO_URL }}/releases/download/v{{ VERSION }}/tera-cli-#{arch}-apple-darwin.tar.gz"
+  sha256 arm:   "{{ AARCH64_SHA256 }}",
+         intel: "{{ X86_64_SHA256 }}"
+
   version "{{ VERSION }}"
 
   def install
