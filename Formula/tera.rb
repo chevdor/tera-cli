6d9548fe4a40fdec4899298236ba5241694ea368
Chevdor <chevdor@gmail.com>
2025-02-08 18:22:23 +0100

    Fix formula

--- Formula/tera.rb
+++ Formula/tera.rb
@@ -1,15 +1,17 @@
 class Tera < Formula
-  arch arm: "aarch64", intel: 'x86_64'
+  desc 'A command line utility written in Rust to render templates using the Tera templating engine'
+  homepage 'https://github.com/chevdor/tera-cli'
+  version '0.5.0'
 
-  desc "A command line utility written in Rust to render templates using the tera templating engine"
-  homepage "https://github.com/chevdor/tera-cli"
-  url "https://github.com/chevdor/tera-cli/releases/download/v0.5.0/tera-cli-#{arch}-apple-darwin.tar.gz"
-  sha256 arm:   "cde318afb9b41c7e4a0cfa7041b3355b49f33c4ebbe96e966017dc6eb93afaf4",
-         intel: "5da9dae6b48ed395804fb804a7a7f2901a6e10d620d40bf7ae67ed4abadbcb29"
-
-  version "0.5.0"
+  if Hardware::CPU.arm?
+    url 'https://github.com/chevdor/tera-cli/releases/download/v0.5.0/tera-cli-aarch64-apple-darwin.tar.gz'
+    sha256 'cde318afb9b41c7e4a0cfa7041b3355b49f33c4ebbe96e966017dc6eb93afaf4'
+  else
+    url 'https://github.com/chevdor/tera-cli/releases/download/v0.5.0/tera-cli-x86_64-apple-darwin.tar.gz'
+    sha256 '5da9dae6b48ed395804fb804a7a7f2901a6e10d620d40bf7ae67ed4abadbcb29'
+  end
 
   def install
-    bin.install "tera"
+    bin.install 'tera'
   end
 end
--- templates/formula.rb
+++ templates/formula.rb
@@ -2,15 +2,17 @@
 {%- set HOMEPAGE = HOMEPAGE | default(value=SITE ~ "/" ~ REPO) -%}
 
 class {{ NAME }} < Formula
-  arch arm: "aarch64", intel: 'x86_64'
-
   desc "{{ DESCRIPTION }}"
   homepage "{{ HOMEPAGE }}"
-  url "{{ REPO_URL }}/releases/download/v{{ VERSION }}/tera-cli-#{arch}-apple-darwin.tar.gz"
-  sha256 arm:   "{{ AARCH64_SHA256 }}",
-         intel: "{{ X86_64_SHA256 }}"
-
   version "{{ VERSION }}"
+
+  if Hardware::CPU.arm?
+    url '{{ REPO_URL }}/releases/download/v{{ VERSION }}/tera-cli-aarch64-apple-darwin.tar.gz'
+    sha256 '{{ AARCH64_SHA256 }}'
+  else
+    url '{{ REPO_URL }}/releases/download/v{{ VERSION }}/tera-cli-x86_64-apple-darwin.tar.gz'
+    sha256 '{{ X86_64_SHA256 }}'
+  end
 
   def install
     bin.install "{{ BIN }}"
